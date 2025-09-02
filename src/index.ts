4a25617be8a0bed152ce6645d970da7e26a93aef
Nikita Maslov <maslov.n.e@gmail.com>
2025-09-03 00:51:17 +0300

    finilize backend

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import { getTemperature } from './controllers/temperature';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8001;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  }),
+);
+
 app.use(express.json());
 
-app.get('/temperature', getTemperature);
+app.get('/api/temperature', getTemperature);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
